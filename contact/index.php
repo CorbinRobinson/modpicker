25a7dc8d63c2199c1da6ed76549e643d0b57ba00
Corbin Robinson <corbinrobinson14@gmail.com>
2021-11-18 17:21:57 -0700

    pathfile changes

---
+++ contact/index.php
@@ -0,0 +1,34 @@
+<head>
+    <meta charset="utf-8">
+    <title>Contact</title>
+    <link rel="stylesheet" href="contact.css">
+</head>
+
+<body>
+    <header>
+        <img id="logo" src="../img/logo.svg" alt="creeper" width="50" height="50">
+        <nav id="menu">
+            <div class="table">
+                <ul id="nav__links">
+                    <li><a href="/html/searchPacksByMod/">Search Packs by Mod</a> &nbsp; &nbsp;</li>
+                    <li><a href="/html/searchModsByPack/">Search Mods by Pack</a> &nbsp; &nbsp;</li>
+                </ul>
+            </div>
+        </nav>
+        <div>
+            <a class="cta" href="/html/contact/"><button>Contact</button></a>
+        </div>
+    </header>
+
+    <body>
+
+        <div>
+            <canvas id="myCanvas" width="500" height="500" style="border:1px solid #000000;">
+            </canvas>
+            <a id="canvasSend"><button>Send us a message!</button></a>
+        </div>
+
+    </body>
+
+    <script src="contact.js"></script>
+</body>
