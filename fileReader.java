e4465beec21342d1f348b59f9d9a275b9d8d1ccb
Corbin Robinson <corbinrobinson14@gmail.com>
2021-11-23 16:56:56 -0700

    createOverallMods added

--- fileReader.java
+++ fileReader.java
@@ -1,15 +1,55 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class fileReader {
     public static void main(String[] args) {
-        //createModlist();
+        // createModlist();
+
+        try {
+            createOverallModlist();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println(e);
+        }
+
     }
 
-    public static void createOverallModlist(){
-        
+    public static void createOverallModlist() throws IOException {
+        File dir = new File("modpackInfo");
+        File[] directoryListing = dir.listFiles();
+        ArrayList<String> modlist = new ArrayList<String>();
+        int overlapCount = 0;
+        if (directoryListing != null) {
+            FileWriter writer = new FileWriter("src\\modpackInfo\\all.txt");
+            for (File child : directoryListing) {
+                // Do something with child
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(child));
+                    String s;
+                    // int i = 0;
+                    while ((s = br.readLine()) != null) {
+                        if (!modlist.contains(s)) {
+                            // writer.write(s + "\n");
+                            modlist.add(s);
+                            // System.out.println(s);
+                        } else {
+                            overlapCount++;
+                        }
+                        // i++;
+                    }
+                    br.close();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+            writer.close();
+        } else {
+            System.out.println("could not find dir");
+        }
+        System.out.println(overlapCount);
     }
 
-    public static void createModlist(){
+    public static void createModlist() {
         File file = new File("modpackInfo\\bleh.txt");
         String newFile = "Mineshafts&Monsters.txt";
         try {
