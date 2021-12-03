11e4ef578e11230be8f8994a96c496f66d4a0925
Corbin Robinson <corbinrobinson14@gmail.com>
2021-12-02 18:09:36 -0700

    started checking modpacks against checkboxes

--- searchPacksByMod/loadModpacks.php
+++ searchPacksByMod/loadModpacks.php
@@ -8,27 +8,32 @@ $result = mysqli_query($conn, $query);
 $mods = json_decode($_POST["mods"], true);
 //TODO: only get packs that match the mod preferences
 if (count($mods) > 0) {
-    foreach ($mods as $modKey => $modValue) {
-        // echo implode(',', $mods);
-        echo $modKey;
-        if ($mods["{$modKey}"] == true) {
+    if ($result->num_rows > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $modpackModlist = $row['modlist'];
+            $flag = true;
+            foreach ($mods as $modKey => $modValue) {
+                if ($modValue == true && !str_contains($modpackModlist, $modKey)) {
+                    $flag = false;
+                    break;
+                } else if ($modValue == false && str_contains($modpackModlist, $modKey)) {
+                    $flag = false;
+                    break;
+                }
+            }
+            echo $flag;
+
             echo '
-                <li>
-                    <p>"true: {$modKey}"</p>
-                </li>
-            ';
-        } else if ($mods["{$modKey}"] == false) {
+                        <li>
+                            <a href="' . $row['url'] . '" target="_blank" >
+                                <img title="' . $row['name'] . '" class="pack" src="data:image/jpeg;base64, ' . base64_encode($row['img']) . '"/>
+                            </a>
+                        </li>
+                    ';
         }
+    } else {
+        echo "sql broked";
     }
-    // while ($row = mysqli_fetch_assoc($result)) {
-    //     echo '
-    //             <li>
-    //                 <a href="' . $row['url'] . '" target="_blank" >
-    //                     <img title="' . $row['name'] . '" class="pack" src="data:image/jpeg;base64, ' . base64_encode($row['img']) . '"/>
-    //                 </a>
-    //             </li>
-    //         ';
-    // }
 } else {
-    echo "notta";
+    echo "no mods selected";
 }
